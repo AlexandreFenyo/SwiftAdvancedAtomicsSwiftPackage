d5eafa6620fd67423f37a3e58d48152053d238b3
Alexandre Fenyo <github@fenyo.net>
2024-04-04 01:35:44 +0200

    update

--- Package.swift
+++ Package.swift
@@ -5,27 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftAdvancedAtomicsSwiftPackage",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         .library(
             name: "SwiftAdvancedAtomicsSwiftPackage",
             targets: ["SwiftAdvancedAtomicsSwiftPackage"]),
-        
     ],
 
     dependencies: [
             .package(name: "SwiftAdvancedAtomicsCPackage", path: "../SwiftAdvancedAtomicsCPackage"),
+            .package(name: "iOSToolsMacros", path: "../iOSToolsMacros"),
         ],
 
     targets: [
         .target(
             name: "SwiftAdvancedAtomicsSwiftPackage",
             dependencies: [
-                "SwiftAdvancedAtomicsCPackage"
+                "SwiftAdvancedAtomicsCPackage",
+                "iOSToolsMacros"
             ]),
         .testTarget(
             name: "SwiftAdvancedAtomicsSwiftPackageTests",
             dependencies: [
-                "SwiftAdvancedAtomicsSwiftPackage"
+                "SwiftAdvancedAtomicsSwiftPackage",
+                "iOSToolsMacros"
             ]),
     ]
 )
